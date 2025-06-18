694e2e5e1fe6f23314919702d6c1e068c74fed22
ratan <ratanhalder60@gmail.com>
2025-06-18 17:58:00 +0600

    create frontEnd

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from core.views import home
+from core.views import home, menu, tracking, reservation, contact
 urlpatterns = [
     path('', home, name='home'),
+    path("menu/", menu, name="menu"),
+    path("order-track/", tracking, name="track"),
+    path("reservation/", reservation, name="reservation"),
+    path("contact/", contact, name="contact"),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(req):
     return render(req, 'core/home.html')
+
+def menu(req):
+    return render(req, 'core/menu.html')
+
+def tracking(req):
+    return render(req, 'core/tracking.html')
+
+def reservation(req):
+    return render(req, 'core/reservation.html')
+
+def contact(req):
+    return render(req, 'core/contact.html')
