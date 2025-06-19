dceafbcdbf25c1929d490a5a8184179b334a3d81
ratan <ratanhalder60@gmail.com>
2025-06-19 10:28:33 +0600

    create for loops for dishes..and create dishes dictionary in view

--- core/views.py
+++ core/views.py
@@ -2,7 +2,36 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(req):
-    return render(req, 'core/home.html')
+    dishes = [
+        {
+            'name' : 'Paneer Butter Masala',
+            'description' : 'Creamy and rich, with a delightful blend of spices, perfect with naan or rice.',
+            'image': 'core/images/paneer_butter_masala.png',
+        },
+        {
+            'name' : 'Sushi',
+            'description' : 'Sushi is a Japanese dish featuring seasoned rice, often combined with seafood (raw or cooked), vegetables, and sometimes wrapped in seaweed.',
+            'image' : 'core/images/sushi.png',
+        },
+        {
+            'name' : 'Butter Paneer',
+            'description' : 'Creamy North Indian curry featuring soft paneer cheese in a tomato-based sauce with butter and spices.',
+            'image' : 'core/images/paneer.png',
+        },
+        {
+            'name' : 'Traditional Brazilian Food',
+            'description' : 'Traditional Brazilian food is delicious, colorful, diverse, and exciting.',
+            'image' : 'core/images/traditional_brazilian_food.png',
+        },
+        {
+            'name' : 'Kacchi Biryani',
+            'description' : 'Biryani is a mixed rice dish that originated in South Asia, featuring fragrant rice (typically basmati) cooked with meat (chicken, goat, lamb, beef, or fish) or vegetables, and a blend of aromatic spices.',
+            'image' : 'core/images/biryani.png',
+        },
+        # Can add more dishes here
+    ]
+
+    return render(req, 'core/home.html', {'dishes': dishes})
 
 def menu(req):
     return render(req, 'core/menu.html')
